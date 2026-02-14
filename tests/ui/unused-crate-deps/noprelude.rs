8b5ee19f0be145489f5bca2a37158ede0f10ab91
biscuitrescue <karttikeyasinha11@gmail.com>
2026-02-14 10:58:11 +0530

    Included standard aux-crate alongside noprelude crate

--- tests/ui/unused-crate-deps/noprelude.rs
+++ tests/ui/unused-crate-deps/noprelude.rs
@@ -1,7 +1,10 @@
 // noprelude --extern references don't count
 //@ edition:2018
 //@ check-pass
+//@ aux-crate:baz=baz.rs
 //@ aux-crate:noprelude:bar=bar.rs
 //@ compile-flags: -Zunstable-options
 #![warn(unused_crate_dependencies)]
+//~^ WARNING extern crate `baz` is unused in crate `noprelude`
+// No warning from bar is expected since it's noprelude
 fn main() {}
